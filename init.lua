89bc6629ec71066654ba7189836a31d958c31a63
Alexandr <83012377+AlexKutz@users.noreply.github.com>
2024-07-30 00:18:50 +0300

    neovim config

---
+++ init.lua
@@ -0,0 +1,230 @@
+-- Set space as leader key
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+local opts = { noremap = true, silent = true }
+
+-- Reindent file
+vim.keymap.set('n', '<C-A-l>', function()
+  local pos = vim.api.nvim_win_get_cursor(0)  
+  vim.cmd("normal! gg=G")                     
+  vim.api.nvim_win_set_cursor(0, pos)        
+end, { silent = true, desc = 'Reindent file' })
+
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Ctrl + s saves the file
+vim.keymap.set("n","<C-s>", ":write<CR>", { silent = true })
+
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Shift + q - Quit
+vim.keymap.set("n", "Q", ":q!<CR>")
+
+-- vv - Makes vertical split
+vim.keymap.set("n", "vv", "<C-W>v")
+-- ss - Makes horizontal split
+vim.keymap.set("n", "ss", "<C-W>s")
+
+-- Quick jumping between splits
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Indenting in visual mode (tab/shift+tab)
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+-- Move to the end of yanked text after yank and paste
+vim.cmd("vnoremap <silent> y y`]")
+vim.cmd("vnoremap <silent> p p`]")
+vim.cmd("nnoremap <silent> p p`]")
+
+-- Space + Space to clean search highlight
+vim.keymap.set("n", "<Leader>h", ":noh<CR>", { silent = true })
+
+-- Fixes pasting after visual selection.
+vim.keymap.set("v", "p", '"_dP')
+
+-- creates a new line below the cursor and goes back into normal mode
+vim.keymap.set('n', '<CR>', 'o<Esc>', opts)
+
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = "a"
+
+-- Sync clipboard between OS and Neovim.
+vim.o.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- No swap files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect"
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- " Open splits on the right and below
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- " update vim after file update from outside
+vim.opt.autoread = true
+
+-- " Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- " Always use spaces insted of tabs
+vim.opt.expandtab = true
+
+-- " Don't wrap lines
+vim.opt.wrap = true
+-- " Wrap lines at convenient points
+vim.opt.linebreak = true
+-- " Show line breaks
+vim.opt.showbreak = "↳"
+
+-- " https://github.com/vim/vim/blob/master/runtime/doc/russian.txt
+-- " Enable hotkeys for Russian layout
+vim.opt.langmap =
+"ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+
+-- " Start scrolling when we'are 8 lines aways from borders
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 15
+vim.opt.sidescroll = 5
+
+-- " This makes vim act like all other editors, buffers can
+-- " exist in the background without being in a window.
+vim.opt.hidden = true
+
+-- " Add the g flag to search/replace by default
+vim.opt.gdefault = true
+
+-- Lazy redraw
+vim.o.lazyredraw = true
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- Theme plugin
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+    },
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup({
+          sort = {
+            sorter = "case_sensitive",
+          },
+          view = {
+            width = 30,
+          },
+          renderer = {
+            group_empty = true,
+          },
+          filters = {
+            dotfiles = true,
+          },
+        })
+      end,
+    },
+    {
+      "karb94/neoscroll.nvim",
+      config = function ()
+        require('neoscroll').setup({})
+      end
+    },
+    -- Smart and Powerful commenting plugin for neovim
+    {
+      "numToStr/Comment.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+      config = function()
+        local comment = require("Comment")
+        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        comment.setup({
+          -- for commenting tsx and jsx files
+          pre_hook = ts_context_commentstring.create_pre_hook(),
+        })
+      end,
+    },
+  }
+})
+
+vim.cmd[[colorscheme tokyonight-night]]
+-- tokyonight-night tokyonight-storm tokyonight-day tokyonight-moon
+--
+neoscroll = require('neoscroll')
+local keymap = {
+  ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 70 }) end;
+  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 70 }) end;
+  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 130 }) end;
+  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 130 }) end;
+}
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
+end
+
