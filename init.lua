e406757cc73fefa404a50f76c6b90f77034909d8
Alexandr <83012377+AlexKutz@users.noreply.github.com>
2024-08-03 00:17:05 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,77 +1,3 @@
--- Set space as leader key
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-local opts = { noremap = true, silent = true }
-
--- Reindent file
-vim.keymap.set('n', '<C-A-l>', function()
-  local pos = vim.api.nvim_win_get_cursor(0)  
-  vim.cmd("normal! gg=G")                     
-  vim.api.nvim_win_set_cursor(0, pos)        
-end, { silent = true, desc = 'Reindent file' })
-
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- Ctrl + s saves the file
-vim.keymap.set("n","<C-s>", ":write<CR>", { silent = true })
-
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Shift + q - Quit
-vim.keymap.set("n", "Q", ":q!<CR>")
-
--- vv - Makes vertical split
-vim.keymap.set("n", "vv", "<C-W>v")
--- ss - Makes horizontal split
-vim.keymap.set("n", "ss", "<C-W>s")
-
--- Quick jumping between splits
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-
--- Indenting in visual mode (tab/shift+tab)
-vim.keymap.set("v", "<Tab>", ">gv")
-vim.keymap.set("v", "<S-Tab>", "<gv")
-
--- Move to the end of yanked text after yank and paste
-vim.cmd("vnoremap <silent> y y`]")
-vim.cmd("vnoremap <silent> p p`]")
-vim.cmd("nnoremap <silent> p p`]")
-
--- Space + Space to clean search highlight
-vim.keymap.set("n", "<Leader>h", ":noh<CR>", { silent = true })
-
--- Fixes pasting after visual selection.
-vim.keymap.set("v", "p", '"_dP')
-
--- creates a new line below the cursor and goes back into normal mode
-vim.keymap.set('n', '<CR>', 'o<Esc>', opts)
-
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Set highlight on search
 vim.o.hlsearch = true
 
@@ -91,6 +17,7 @@ vim.o.breakindent = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
+
 -- Save undo history
 vim.o.undofile = true
 
@@ -156,6 +83,25 @@ vim.opt.gdefault = true
 -- Lazy redraw
 vim.o.lazyredraw = true
 
+
+-- Plugin manager install
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
@@ -166,6 +112,7 @@ require("lazy").setup({
       priority = 1000,
       opts = {},
     },
+    -- File tree extension
     {
       "nvim-tree/nvim-tree.lua",
       version = "*",
@@ -190,6 +137,7 @@ require("lazy").setup({
         })
       end,
     },
+    -- Smooth scrolling extension
     {
       "karb94/neoscroll.nvim",
       config = function ()
@@ -210,21 +158,114 @@ require("lazy").setup({
         })
       end,
     },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    }
   }
 })
 
-vim.cmd[[colorscheme tokyonight-night]]
--- tokyonight-night tokyonight-storm tokyonight-day tokyonight-moon
---
+vim.cmd.colorscheme('tokyonight-night')
+-- Other themes to change: 
+-- tokyonight-night tokyonight-storm 
+-- tokyonight-day tokyonight-moon
+
+-- Open file tree
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Configure scroll
 neoscroll = require('neoscroll')
 local keymap = {
   ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 70 }) end;
-  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 70 }) end;
-  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 130 }) end;
-  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 130 }) end;
+  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 50 }) end;
+  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 100 }) end;
+  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 100 }) end;
 }
 local modes = { 'n', 'v', 'x' }
 for key, func in pairs(keymap) do
   vim.keymap.set(modes, key, func)
 end
+
+-- Set space as leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Define options for keymap
+local opts = { noremap = true, silent = true }
+
+-- Reindent file
+vim.keymap.set('n', '<C-A-l>', function()
+  local pos = vim.api.nvim_win_get_cursor(0)  
+  vim.cmd("normal! gg=G")                     
+  vim.api.nvim_win_set_cursor(0, pos)        
+end, { silent = true, desc = 'Reindent file' })
+
+-- Make space key don't do anything
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Ctrl + s saves the file
+vim.keymap.set("n","<C-s>", ":write<CR>", { silent = true })
+
+-- I have no idea what this does
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Shift + q - Quit
+vim.keymap.set("n", "Q", ":q!<CR>")
+
+-- vv - vertical split
+vim.keymap.set("n", "vv", "<C-W>v")
+
+-- ss - horizontal split
+vim.keymap.set("n", "ss", "<C-W>s")
+
+-- Quick jumping between splits
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Indenting in visual mode (tab/shift+tab)
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+-- Copy / paste from clipboard
+vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p', {desc = 'Paste clipboard content'})
+
+-- Not write text to copy register when delete characters
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+vim.keymap.set({'n', 'x'}, 'X', '"_d')
+
+
+-- Select all text
+vim.keymap.set('n', '<C-a>', ':keepjumps normal! ggVG<cr>')
+
+-- Move to the end of yanked text after yank and paste
+vim.cmd("vnoremap <silent> y y`]")
+vim.cmd("vnoremap <silent> p p`]")
+vim.cmd("nnoremap <silent> p p`]")
+
+-- Space + Space to clean search highlight
+vim.keymap.set("n", "<Leader>h", ":noh<CR>", { silent = true })
+
+-- Fixes pasting after visual selection.
+vim.keymap.set("v", "p", '"_dP')
+
+-- creates a new line below the cursor and goes back into normal mode
+vim.keymap.set('n', '<CR>', 'o<Esc>', opts)
 
